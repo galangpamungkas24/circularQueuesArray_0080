81740fe5108e4625fac25b7974dbabda7e4ba58f
galang <ahmad.galang.pamungkas24@gmail.com>
2025-06-12 17:51:24 +0700

    cek apakah antrian kosong

--- Circular.cpp
+++ Circular.cpp
@@ -42,4 +42,16 @@ public:
             else
                 REAR = REAR + 1;
         }
+         queue_array[REAR] = num;
+    }
+
+    void remove()
+    { 
+        // cek apakah antrian kosong
+        if (FRONT == -1)
+        {
+            cout << "Queue underflow\n";
+            return;
+        }
+
 
